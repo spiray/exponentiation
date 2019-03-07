9db18cee63628d5b968e82a481170440edb8fa35
Yosef Spira <spira.joe214@gmail.com>
2019-03-07 14:07:50 -0500

    version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function exponentiation(num, exp) {
     const base = Number(num);
     const exponent = Number(exp);
-    if (typeof base !== 'number' || typeof exponent !== 'number') throw new Error('Both arguments must be numbers.')
+    if (!Number.isInteger(base) || !Number.isInteger(exponent)) throw new Error('Both arguments must be numbers.')
     try {
         return base ** exponent;
     } catch (err) {
